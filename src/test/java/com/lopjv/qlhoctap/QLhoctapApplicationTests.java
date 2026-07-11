a602de8974cb78a626a104a7127108bdc1bafe3b
HoangLeDuc2304 <DucHL.B22CN224@stu.ptit.edu.vn>
2026-07-11 17:06:26 +0700

    123

---
+++ src/test/java/com/lopjv/qlhoctap/QLhoctapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopjv.qlhoctap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QLhoctapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
